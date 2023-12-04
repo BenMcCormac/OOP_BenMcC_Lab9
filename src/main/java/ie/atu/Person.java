4f72bebb4fcaae964bbac6c5cc2466186b2ec6fc
BenMcCormack <g00416547@atu.ie>
2023-12-04 09:24:59 +0000

    Constructors, Getters and Setters

--- src/main/java/ie/atu/Person.java
+++ src/main/java/ie/atu/Person.java
@@ -2,5 +2,45 @@ package ie.atu;
 
 public class Person
 {
+    private String name;
+    private String address;
+    private String teleNumber;
 
+    public Person ()
+    {
+        this.address = "";
+        this.name = "";
+        this.teleNumber = "";
+    }
+
+    public Person(String name, String address, String teleNumber)
+    {
+        this.address = "address";
+        this.name = "name";
+        this.teleNumber = "telenumber";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTeleNumber() {
+        return teleNumber;
+    }
+
+    public void setTeleNumber(String teleNumber) {
+        this.teleNumber = teleNumber;
+    }
 }
